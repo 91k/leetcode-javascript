5cc2abcf916feaab4a7e157bb892fa82d516fefd
91k <11503318+91k@users.noreply.github.com>
2021-08-04 16:28:22 +0300

    Update 338-counting-bits.js

--- 338-counting-bits.js
+++ 338-counting-bits.js
@@ -6,7 +6,7 @@ var countBits = function (n) {
     var ans = new Array(n + 1).fill(0);
 
     for (var i = 0; i <= n; i++) {
-        ans[i] = ans[i >> 1] + (i & 1);
+        ans[i] = ans[i >> 1] + i % 2;
     }
 
     return ans;
