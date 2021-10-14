43f7b64e525333c273fa07ac422180a815698c90
Roman Kosov <11503318+91k@users.noreply.github.com>
2021-10-14 17:09:04 +0300

    Update 268-missing-number.js

--- 268-missing-number.js
+++ 268-missing-number.js
@@ -4,7 +4,7 @@
  */
 var missingNumber = function (nums) {
     var n = nums.length;
-    var arit = (n + 1) / 2 * n // Link: https://ru.wikipedia.org/wiki/Арифметическая_прогрессия#Сумма_первых_%7F'"`UNIQ--postMath-0000003C-QINU`"'%7F_членов_арифметической_прогрессии
+    var arit = (n + 1) / 2 * n; // Link: https://ru.wikipedia.org/wiki/Арифметическая_прогрессия#Сумма_первых_%7F'"`UNIQ--postMath-0000003C-QINU`"'%7F_членов_арифметической_прогрессии
     var sum = nums.reduce((a, b) => a + b);
 
     return arit - sum;
