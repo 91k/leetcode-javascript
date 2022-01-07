5830c2ddea02d5b1cb689054f5cf51b088dd9c25
Roman Kosov <11503318+91k@users.noreply.github.com>
2022-01-07 20:51:24 +0300

    Update 1470-shuffle-the-array.js

--- 1470-shuffle-the-array.js
+++ 1470-shuffle-the-array.js
@@ -5,11 +5,10 @@
  */
 var shuffle = function(nums, n) {
     var len = nums.length;
-    var mid = len / 2;
     var result = [];
     var index = 0;
     
-    for (var i = 0; i < mid; i++) {
+    for (var i = 0; i < n; i++) {
         result[index] = nums[i];
         result[index+1] = nums[i+n];
         index+=2;
